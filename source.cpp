1079c10df8bfdd2f19ee4c2f30ae547ac03a3302
Konstantin <me@minedomain.com>
2014-02-19 21:39:10 +0400

    another release with new dinamical information instead of first constant size 255+16 for eash file

--- source.cpp
+++ source.cpp
@@ -10,10 +10,16 @@
 #define DEFAULT_MODE S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
 const size_t FILESIZESTRING_MAX = 16;
 
-//new format of archive
-// <name of file><size of file><data of file>
-// <name of file> = Name_max characters
-// <size of file> = FILESIZESTRING_MAX characters
+// format of archive
+// <length of name of file><name of file><length of size of file><size of file><data of file>
+// <length of name of file> = 1 byte
+// <name of file> = <length of name of file> characters
+// <length of size of file> = 1 byte
+// <size of file> = <length of size of file> characters
+
+char *inttobytes (int number);
+int bytestoint (char *s);
+long int pow_int (int x, int y);
 
 int main(int argc,char *argv[]) {
 	DIR *dfd;
@@ -23,6 +29,8 @@ int main(int argc,char *argv[]) {
 	char foldername[NAME_MAX];
 	char tempchar[NAME_MAX];
 	char buffer[BUF_SIZE];
+	char bytesname;
+	char *stroka;
 	ssize_t read_bytes;
 	ssize_t data_read_bytes;
 	ssize_t data_need_bytes;
@@ -31,10 +39,9 @@ int main(int argc,char *argv[]) {
 	int out_file_d;
 	struct stat statfile; 
 	if (argc!=4) {
-		printf("Arguments requared.\narchivator -glue/-unglue ");
+		printf("Arguments required.\narchivator -glue/-unglue ");
 		printf("input_folder/input_file output_file/output_folder\n");
-	}
-	else {
+	} else {
 		if (strcmp(argv[1],"-glue")==0) {
 			printf("Glue operation is started.\n");
 			strcpy(foldername, argv[2]);
@@ -53,11 +60,18 @@ int main(int argc,char *argv[]) {
 					memset(tempchar,0,sizeof(tempchar));
 					sprintf(tempchar,"%s", dp->d_name);
 					//printf(" %s", tempchar);
-					written_bytes=write(out_file_d, tempchar, NAME_MAX);
+					bytesname=strlen(tempchar);
+					//printf("%d\n",bytesname);
+					written_bytes=write(out_file_d, &bytesname, 1);
+					written_bytes=write(out_file_d, tempchar, bytesname);
 					memset(tempchar,0,sizeof(tempchar));
 					sprintf(tempchar,"%lld", (long long)statfile.st_size);
 					//printf(" %s", tempchar);
-					written_bytes=write(out_file_d, tempchar, FILESIZESTRING_MAX);
+					stroka=inttobytes((long long)statfile.st_size);
+					//printf("%s\n%d\n",stroka,bytestoint(stroka));
+					bytesname=strlen(stroka);
+					written_bytes=write(out_file_d, &bytesname, 1);
+					written_bytes=write(out_file_d, stroka, bytesname);
 					data_read_bytes=(size_t)0;
 					do {
 						read_bytes=read(inp_file_d, buffer,BUF_SIZE);
@@ -79,19 +93,25 @@ int main(int argc,char *argv[]) {
 			//dfd=opendir(foldername);
 			mkdir(foldername,DEFAULT_MODE);
 			inp_file_d=open(filename, O_RDONLY);
+			memset(tempchar,0,sizeof(tempchar));
 			do {
-				memset(tempchar,0,sizeof(tempchar));
-				read_bytes=read(inp_file_d, tempchar,NAME_MAX);
+				read_bytes=read(inp_file_d, tempchar,1);
 				if (read_bytes==(size_t)0) break;
+				bytesname=tempchar[0];
+				memset(tempchar,0,sizeof(tempchar));
+				read_bytes=read(inp_file_d, tempchar,bytesname);
 				memset(absfilename,0,sizeof(absfilename));
 				sprintf(absfilename,"./%s/%s", foldername, tempchar);
 				out_file_d=open(absfilename, O_RDWR|O_CREAT, S_IWUSR | S_IRUSR);
 				printf("Unclue file %s.\n", absfilename);
+				read_bytes=read(inp_file_d, tempchar,1);
+				bytesname=tempchar[0];
 				memset(tempchar,0,sizeof(tempchar));
-				read_bytes=read(inp_file_d, tempchar,FILESIZESTRING_MAX);
+				read_bytes=read(inp_file_d, tempchar,bytesname);
 				//printf("%s.\n", tempchar);
 				long long tempbytes=0;
-				sscanf(tempchar,"%lld",&tempbytes);
+				//sscanf(tempchar,"%lld",&tempbytes);
+				tempbytes=bytestoint(tempchar);
 				data_need_bytes=(size_t) tempbytes;
 				printf("%lld bytes.\n", (long long)data_need_bytes);
 				//printf("debug\n");
@@ -117,22 +137,56 @@ int main(int argc,char *argv[]) {
 	}
 	return 0;
 }
-/*
-char putspace[NAME_MAX] (char *inputchar[],size_t needsize, int flag)
-{
-	char tempchar[NAME_MAX];
-	size_t count;
-	switch (flag) {
-		case 0: {
-			count=needsize-strlen(inputchar);
-			sprintf(tempchar, "%s", inputchar);
-		}
-		case 1: {
-		}
-		default: {
-			printf("Error.");
-		}
-	}
-	return tempchar;
+
+char *inttobytes (int inp_number) {
+	char s[256];
+	char letter=0;
+	int number=inp_number;
+	int buf=0;
+	int pos=0;
+	memset(s,0,sizeof(s));
+	do {
+		buf=number%256;
+		if (buf==0) number=0;
+		letter=buf;
+		s[pos]=letter;
+		pos++;
+	} while((number=number/256)!=0);
+	char s_inv[256];
+	int i=0;
+	memset(s_inv,0,sizeof(s_inv));
+	pos--;
+	do {
+		s_inv[i]=s[pos];
+		i++;
+		pos--;
+	} while(pos!=-1);	
+	char *outs;
+	outs=s_inv;
+	return outs;
 }
-*/
+
+int bytestoint (char *s) {
+	int l=strlen(s);
+	int buf=0;
+	int pos=0;
+	int rezult=0;
+	unsigned char utemp=0;
+	while(l!=0) {
+		utemp=s[l-1];
+		buf=utemp*pow_int(256, pos);
+		rezult+=buf;
+		l--;
+		pos++;
+	}
+	return rezult;
+}
+
+long int pow_int(int x,int y) {
+	long int rezult=1;
+	while(y!=0) {
+		rezult*=x;
+		y--;
+	}
+	return rezult;
+}
