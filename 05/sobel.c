13cf2e4cdb230743334f73f3cca6fa32adeafb95
Konstantin Iliasov <kost1993@yandex.ru>
2014-05-08 22:50:23 +0400

    Добавлена проверка для malloc, вынесены объявления функций в h файл

--- 05/sobel.c
+++ 05/sobel.c
@@ -7,10 +7,19 @@
 #include <fcntl.h> /* open(), O_RDONLY, O_WRONLY */
 #include <unistd.h> /* read(), write(), close() */
 #include <pthread.h>
+#include <sys/time.h> /* gettimeofday() */
+#include "sobel.h"
 
 #define BUFSIZE 4096
 #define LEN_STR_NORM 80
 #define OUTPUT_FORMAT 5
+
+#define SAFE_MALLOC(v, t, n) \
+do { \
+	v = (t *)malloc((n)*sizeof(t)); \
+	if (v == NULL) \
+		return EXIT_FAILURE; \
+} while (0)
 
 struct RGB {
 	unsigned char R;
@@ -36,74 +45,77 @@ int gx[3][3] = {{-1, 0, 1},
 		{-2, 0, 2},
 		{-1, 0, 1} };
 
-void *work(void *number);
-void free_mem();
-
 int main(int argc, char *argv[])
 {
+	struct timeval start_time, end_time;
+	double time_elapsed;
 	int ALLOWED_NUM_THREADS;
 	int index_thread;
-	ALLOWED_NUM_THREADS = 3;
-	id = malloc(sizeof(pthread_t) * ALLOWED_NUM_THREADS);
-	massive_of_indexes_thread = malloc(sizeof(int) * ALLOWED_NUM_THREADS);
-	position_thread_start = malloc(sizeof(int) * ALLOWED_NUM_THREADS);
-	position_thread_end = malloc(sizeof(int) * ALLOWED_NUM_THREADS);
-	if (argc != 3) {
-		printf("Usage:\nsobel input_file.ppm output_file.pgm\n");
-		return 1;
+	int count;
+	if (argc != 4) {
+		printf("Usage:\nsobel input_file.ppm output_file.pgm count_threads\n");
+		return EXIT_FAILURE;
 	}
+	count = sscanf(argv[3], "%d", &ALLOWED_NUM_THREADS);
+	if (count != 1) {
+		printf("Cant recognize count of threads\n");
+		return EXIT_FAILURE;
+	}
+	SAFE_MALLOC(id, pthread_t, ALLOWED_NUM_THREADS);
+	SAFE_MALLOC(massive_of_indexes_thread, int, ALLOWED_NUM_THREADS);
+	SAFE_MALLOC(position_thread_start, int, ALLOWED_NUM_THREADS);
+	SAFE_MALLOC(position_thread_end, int, ALLOWED_NUM_THREADS);
 	int in_fd, out_fd;
 	int offset_start = 0;
 	in_fd = open(argv[1], O_RDONLY);
 	if (in_fd == -1) {
 		printf("Error open input file\n");
-		return 1;
+		return EXIT_FAILURE;
 	}
 	out_fd = creat(argv[2], S_IWUSR | S_IRUSR);
 	if (out_fd == -1) {
 		printf("Error creat output file\n");
-		return 1;
+		return EXIT_FAILURE;
 	}
 	FILE *in_stream;
 	in_stream = fdopen(in_fd, "r");
 	if (in_stream == NULL) {
 		printf("Error open input stream file\n");
-		return 1;
+		return EXIT_FAILURE;
 	}
 	char buf[BUFSIZE];
 	if (fgets(buf, 4, in_stream) == NULL) {
 		printf("Error read input file\n");
-		return 1;
+		return EXIT_FAILURE;;
 	}
 	offset_start += strlen(buf);
 	char mode_image[2];
 	strncpy(mode_image, buf, 2);
 	if (strncmp(mode_image, "P6", 2) != 0) {
 		printf("Please use images in format ppm version p6\n");
-		return 1;
+		return EXIT_FAILURE;;
 	}
 	memset(buf, 0, BUFSIZE);
 	sprintf(buf, "P%d\n", OUTPUT_FORMAT);
 	write(out_fd, buf, strlen(buf));
 	if (fgets(buf, LEN_STR_NORM, in_stream) == NULL) {
 		printf("Error read input file\n");
-		return 1;
+		return EXIT_FAILURE;
 	}
 	offset_start += strlen(buf);
 	while (buf[0] == '#') {
 		memset(buf, 0, BUFSIZE);
 		if (fgets(buf, LEN_STR_NORM, in_stream) == NULL) {
 			printf("Error read input file\n");
-			return 1;
+			return EXIT_FAILURE;
 		}
 		offset_start += strlen(buf);
 	}
 
-	int count;
 	count = sscanf(buf, "%d %d", &width, &height);
 	if (count != 2) {
 		printf("Cant get information about resolution\n");
-		return 1;
+		return EXIT_FAILURE;
 	}
 	memset(buf, 0, BUFSIZE);
 	sprintf(buf, "%d %d\n", width - 2, height - 2);
@@ -111,7 +123,7 @@ int main(int argc, char *argv[])
 	memset(buf, 0, BUFSIZE);
 	if (fgets(buf, LEN_STR_NORM, in_stream) == NULL) {
 		printf("Cant get information about brightness\n");
-		return 1;
+		return EXIT_FAILURE;
 	}
 	offset_start += strlen(buf);
 	count = sscanf(buf, "%d", &brightness);
@@ -123,21 +135,25 @@ int main(int argc, char *argv[])
 	n_kk = 3;
 	int size_pic;
 	size_pic = width * height * n_kk;
-	unsigned char *allPix = (unsigned char *)malloc(size_pic * sizeof(unsigned char));
+	unsigned char *allPix;
+	SAFE_MALLOC(allPix, unsigned char, size_pic);
 
-	allPixRGB = (struct RGB **)malloc(width * sizeof(struct RGB *));
+	SAFE_MALLOC(allPixRGB, struct RGB *, width);
 	for (i = 0; i < width; i++)
-		allPixRGB[i] = (struct RGB *)malloc(height * sizeof(struct RGB));
+		SAFE_MALLOC(allPixRGB[i], struct RGB, height);
 
-	outPix = (unsigned char **)malloc((width - 2) * sizeof(unsigned char *));
-	for (i = 0; i < width - 2; i++)
-		outPix[i] = (unsigned char *)malloc((height - 2) * sizeof(unsigned char));
+	SAFE_MALLOC(outPix, unsigned char *, (height - 2));
+	for (j = 0; j < height - 2; j++)
+		SAFE_MALLOC(outPix[j], unsigned char, (width - 2));
 	int offset_check;
 	offset_check = lseek(in_fd, offset_start, 0);
 	int read_bytes;
 	read_bytes = read(in_fd, allPix, size_pic * sizeof(unsigned char));
+	if (read_bytes == -1) {
+		printf("Error to read matrix of pixels\n");
+		return EXIT_FAILURE;
+	}
 	fclose(in_stream);
-	/*close(in_fd);*/
 	int index;
 	index = 0;
 	for (j = 0; j < height; j++) {
@@ -168,6 +184,7 @@ int main(int argc, char *argv[])
 	}
 
 	index_thread = 0;
+	gettimeofday(&start_time, NULL);
 	do {
 		massive_of_indexes_thread[index_thread] = index_thread;
 		if (pthread_create(&id[index_thread],
@@ -184,42 +201,23 @@ int main(int argc, char *argv[])
 		pthread_join(id[index_thread], NULL);
 		index_thread--;
 	} while (index_thread >= 0);
-
-	unsigned char rezult;
-	int size_rezult = 8;
-	rezult = 0;
-	index = 0;
-	memset(buf, 0, BUFSIZE);
-	for (j = 0; j < height - 2; j++) {
-		for (i = 0; i < width - 2; i++) {
-			write(out_fd, &outPix[i][j], sizeof(char));
-			/*rezult += outPix[i][j];
-			if (index == size_rezult-1) {
-				write(out_fd, &rezult, sizeof(rezult));
-				rezult = 0;
-				index = 0;
-				continue;
-			}
-			rezult = rezult << 1;
-			index++;*/
-		}
-		/*while (index != size_rezult-1) {
-			rezult = rezult << 1;
-			index++;
-		}
-		write(out_fd, &rezult, sizeof(rezult));
-		rezult = 0;
-		index = 0;*/
-	}
-	write(out_fd, &rezult, sizeof(rezult));
+	gettimeofday(&end_time, NULL);
+	/*sec to ms*/
+	time_elapsed = (end_time.tv_sec - start_time.tv_sec) * 1000.0;
+	/*us to ms*/
+	time_elapsed += (end_time.tv_usec - start_time.tv_usec) / 1000.0;
+	printf("Time elapsed %g ms\n", time_elapsed);
+	for (j = 0; j < height - 2; j++)
+		write(out_fd, outPix[j], (width - 2) * sizeof(char));
 	close(out_fd);
-	for (i = 0; i < width - 2; i++)
-		free(outPix[i]);
+	for (j = 0; j < height - 2; j++)
+		free(outPix[j]);
 	free(outPix);
 	free(allPix);
 	for (i = 0; i < width; i++)
 		free(allPixRGB[i]);
 	free(allPixRGB);
+	free_mem();
 	return 0;
 }
 
@@ -240,8 +238,6 @@ void *work(void *number)
 	int rc, gc, bc;
 	int wi, hw;
 	int i, j;
-	/*for (j = 1; j < height - 1; j++) {*/
-
 	for (j = position_thread_start[local_index];
 	     j < position_thread_end[local_index]; j++) {
 		for (i = 1; i < width - 1; i++) {
@@ -269,16 +265,10 @@ void *work(void *number)
 			rc = sqrt(new_rx * new_rx + new_ry * new_ry);
 			gc = sqrt(new_gx * new_gx + new_gy * new_gy);
 			bc = sqrt(new_bx * new_bx + new_by * new_by);
-			/*outPix[i - 1][j - 1] = 0.299 * rc + 0.587 * gc + 0.114 * bc;*/
-			outPix[i - 1][j - 1] = 0.2126 * rc + 0.7152 * gc + 0.0722 * bc;
-			if (outPix[i - 1][j - 1] > brightness)
-				outPix[i - 1][j - 1] = brightness;
-			/*if (new_rx * new_rx + new_ry * new_ry > limit ||
-				new_gx * new_gx + new_gy * new_gy > limit ||
-				new_bx * new_bx + new_by * new_by > limit)
-				outPix[i - 1][j - 1] = 255;
-			else
-				outPix[i - 1][j - 1] = 0;*/
+			/*outPix[j - 1][i - 1] = 0.299 * rc + 0.587 * gc + 0.114 * bc;*/
+			outPix[j - 1][i - 1] = 0.2126 * rc + 0.7152 * gc + 0.0722 * bc;
+			if (outPix[j - 1][i - 1] > brightness)
+				outPix[j - 1][i - 1] = brightness;
 		}
 	}
 }
